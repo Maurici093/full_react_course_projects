d48d89be217aa9a8c458cb5c5235cd341536a782
Maurici093 <bryamvaron@gmail.com>
2022-08-02 09:46:43 -0500

    add advance in rolodex project

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,41 @@
+import {Component} from 'react';
+import './App.css';
+
+
+class App extends Component {
+
+  constructor() {
+    super();
+    
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=>response.json())
+      .then(users=>this.setState(
+          ()=> {
+            return {monsters: users}
+          },
+          ()=> {
+            console.log(this.state)
+          })
+      )
+  }
+
+  render() {
+    return (
+      <div className="App">
+         {
+          this.state.monsters.map((monster)=>{
+            return <h1 key={monster.id}>{monster.name}</h1>;
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
