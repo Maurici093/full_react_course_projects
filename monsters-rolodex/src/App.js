81213f932fad9faaa5066c977c75ba129cf50fbd
Maurici093 <bryamvaron@gmail.com>
2022-08-03 12:51:56 -0500

    implement basic input search

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response=>response.json())
       .then(users=>this.setState(
@@ -28,6 +29,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          className='search-box' 
+          type='search' 
+          placeholder='search moster'
+          onChange={(event) => {
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString)
+            });
+
+            this.setState(() => {
+              return {monsters: filteredMonsters}
+            })
+          }} 
+        />
          {
           this.state.monsters.map((monster)=>{
             return <h1 key={monster.id}>{monster.name}</h1>;
